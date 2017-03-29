2399f9d385e786876a4fe46e8ab0ace5b047c9e4
David Arteaga <david17art@gmail.com>
2017-03-29 15:09:28 -0400

    project

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name = "tinyintError",
+	packages = ["package"],
+	version = "0.1",
+	descripcion = "Este paquete nos permite validad errores de tipo tinyInt",
+	author = "David Arteaga",
+	author_email = "david17art@gmail.com",
+	url = "",
+	keyword = ["int","tinyint","errores","validates"]
+
+)
